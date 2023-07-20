ae21342bf2e3552e4a3af454c35c9323939b10fe
Alexandra Pereira <alesilva241@gmail.com>
2023-07-19 21:22:04 -0400

    add card layout
    include style and layout to present the books.
    
    Signed-off-by: Alexandra Pereira <alesilva241@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import React from "react";
 
+import Card from "./components/layout/Card";
 import BookList from "./components/BookList.jsx";
 
 export default (_) => {
   return (
     <div className="App">
       <h1>Current Manga Best Sellers</h1>
-      <BookList/>
+        <div className="Cards">
+          <Card title="Mangas" color="#85C1E9">
+            <BookList/>
+          </Card>
+        </div>
     </div>
   );
 };
